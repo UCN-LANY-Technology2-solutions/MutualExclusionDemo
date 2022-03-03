d18a4cee6b0155b964fb74f41690479d06462242
Lars Nysom <LANY@ucn.dk>
2022-03-03 13:12:54 +0100

    Changed buffers to run at constant speeds

--- src/UnsynchronizedBuffer.java
+++ src/UnsynchronizedBuffer.java
@@ -5,24 +5,22 @@ public class UnsynchronizedBuffer implements Bufferable {
 	
 	@Override
 	public int setValue(int value) throws InterruptedException {
-		Thread.sleep((long) (Math.random() * 3001));
-
+		Thread.sleep(1000);
 		if (this.value == 0) {
 			this.value = value;
 			return this.value;
 		}
+		System.out.print("Buffer full! - ");
 		return 0;
 	}
 
 	@Override
 	public int getValue() throws InterruptedException {
-		Thread.sleep((long) (Math.random() * 3001));
-
-		//System.out.println(Thread.currentThread().getName() + " thread reads value " + this.value);
-
+		Thread.sleep(2000);
 		int res = this.value;
 		this.value = 0;
-
+		if(res == 0)
+			System.out.print("Buffer empty - ");
 		return res;
 	}
 
