36b777299fa980730e0dbb25d8fdfcae284b4dd2
Lars Nysom <LANY@ucn.dk>
2022-03-11 10:00:42 +0100

    Added wait notify

--- src/SynchronizedWaitingNotifyBuffer.java
+++ src/SynchronizedWaitingNotifyBuffer.java
@@ -1,24 +1,31 @@
 // Implementation of a synchronized buffer
-public class SynchronizedBuffer implements Bufferable {
+public class SynchronizedWaitingNotifyBuffer implements Bufferable {
 
 
 	private int value = 0;
 
 	public synchronized int setValue(int value) throws InterruptedException {
+
 		Thread.sleep(100);
-		if (this.value == 0) {
-			this.value = value;
-			return this.value;
+
+		while (this.value != 0) {
+			wait();
 		}
-		return 0;	
+		this.value = value;
+		notify();
+		return this.value;	
 	}
 
 	public synchronized int getValue() throws InterruptedException {
+
 		Thread.sleep(200);
+
+		while (this.value == 0) {
+			wait();
+		}
 		int res = this.value;
 		this.value = 0;
-		if(res == 0)
-			System.out.print("Buffer empty - ");
+		notify();
 		return res;
 	}
 
